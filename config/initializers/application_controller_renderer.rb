3c4d6631352566fbeaf073aed715f8208e5a7f5b
hiroyoshi-ueda <torikara.h1139@gmail.com>
2020-02-12 09:43:58 +0000

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8953f23e522f455777b02505363dd394b359bfecd83af7d29e19d23431e4b20f6e6a3a4bfae2991913be8ed2c487e20d707dcb3c90fa15aabf0cad27a8bc8d2c'
